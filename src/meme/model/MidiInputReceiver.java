67479f8214d4dcac5463328770708d33a5b1761c
Derek Vawdrey <flyingpiechicken@gmail.com>
2017-09-19 18:56:58 -0600

    MidiInputReceiver now registers when a key has been pressed/released

--- src/meme/model/MidiInputReceiver.java
+++ src/meme/model/MidiInputReceiver.java
@@ -1,5 +1,8 @@
 package meme.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.sound.midi.MidiMessage;
 import javax.sound.midi.Receiver;
 
@@ -8,7 +11,7 @@ public class MidiInputReceiver implements Receiver
 	
 	private String name;
 	private int number;
-	
+	private List<Byte> KeysOn = new ArrayList<Byte>();
 	
 	public MidiInputReceiver(String name) {
 		this.name = name;
@@ -20,8 +23,28 @@ public class MidiInputReceiver implements Receiver
 		//message.getMessage()
 		//https://www.midi.org/specifications/item/table-1-summary-of-midi-message
 		// TODO Auto-generated method stub
+		
+		byte keyByte = message.getMessage()[1];
+		
 		if(message.getLength() > 1) {
-			System.out.printf("%02X\n", message.getMessage()[1]);
+			if(KeysOn.contains(keyByte)) {
+				//key has been released.				
+				
+				for(int i = 0; i < KeysOn.size(); i++) {
+					if(KeysOn.get(i).equals(keyByte)) {
+						KeysOn.remove(i);
+						System.out.println("Key Released");
+						break;
+					}
+				}
+				
+			}else {
+				//Key has been pressed
+				KeysOn.add(keyByte);
+				System.out.println("Key pressed");
+			}
+			
+			//System.out.printf("%02X\n", message.getMessage()[1]);
 			
 			number = 1;
 			//System.out.println(message.getMessage());
