a432155bc93fda5ae54d551c15c5f37626ef481f
Derek Vawdrey <flyingpiechicken@gmail.com>
2017-09-27 13:13:13 -0600

    added methods to noteBlock class

--- src/meme/model/NoteBlock.java
+++ src/meme/model/NoteBlock.java
@@ -4,11 +4,27 @@ import java.awt.Graphics;
 
 public class NoteBlock
 {
-	NoteBlock(){
-		
+	private double X;
+	private double Y;
+	private double width;
+	private double height;
+	private int keyType;
+	private boolean playing;
+	
+	NoteBlock(int keyType){
+		this.keyType = keyType;
 	}
 	
 	public void draw(Graphics g) {
+		if(playing == false) {
+			Y++;
+		}else {
+			height++;
+		}
 		
+		//Draw noteBlock
 	}
+	
+	
+	
 }
